4754a4e785cdd5274e1a0ff400a19f92efba19bc
Anyth3nG <daniel2233x@gmail.com>
2026-07-17 00:10:23 +0300

    feat: add company logo and name + fix a couple of bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -51,7 +51,16 @@ function RoleRedirect() {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider
+      publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      afterSignOutUrl="/"
+      appearance={{
+        layout: {
+          logoImageUrl: '/logo_mark.svg',
+          logoPlacement: 'inside',
+        },
+      }}
+    >
       <BrowserRouter>
         <Routes>
           <Route path="/sign-in/*" element={<SignInPage />} />
